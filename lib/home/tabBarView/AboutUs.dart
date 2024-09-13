62400bc32e9ea503b30af444e6fb10ee7860d540
Syed Abdal Shah <syedahmad123123shah@gmail.com>
2024-09-14 02:30:46 +0500

    sttuff

--- lib/home/tabBarView/AboutUs.dart
+++ lib/home/tabBarView/AboutUs.dart
@@ -5,7 +5,7 @@ import 'package:app/utils/config.dart';
 import 'package:app/utils/helper.dart';
 import 'package:app/utils/responsive_handler.dart';
 import 'package:app/utils/strings.dart';
-import 'package:carousel_slider/carousel_slider.dart' as CS;
+import 'package:flutter/src/material/carousel.dart' as CS;
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
@@ -101,17 +101,19 @@ class _AboutUsState extends State<AboutUs> {
             defaultValue: 20,
           ),
           SizedBox(
-              height: 400,
-              width: 400,
-              child: CS.CarouselSlider.builder(
-                itemCount: Helper.sliderItems.length,
-                itemBuilder: (context, index, pageindex) => Column(
+            height: 400,
+            width: 400,
+            child: CS.CarouselView(
+              itemExtent: Helper.sliderItems.length.toDouble(),
+              children: List.generate(
+                Helper.sliderItems.length,
+                (index) => Column(
                   children: [
                     const VerticalGap(
                       customGap: 30,
                     ),
                     Helper.sliderItems[index],
-                    VerticalGap(),
+                    const VerticalGap(),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: List.generate(
@@ -128,27 +130,57 @@ class _AboutUsState extends State<AboutUs> {
                     )
                   ],
                 ),
-                options: CS.CarouselOptions(
-                    height: 400,
+              ),
+            ),
+          ),
 
-                    // aspectRatio: 16 / 9,
-                    viewportFraction: 0.8,
-                    initialPage: 0,
-                    enableInfiniteScroll: true,
-                    reverse: false,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 5),
-                    autoPlayAnimationDuration: const Duration(seconds: 1),
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enlargeCenterPage: true,
-                    onPageChanged: (index, point) {
-                      setState(() {
-                        carosalIndex = index;
-                      });
-                    },
-                    scrollDirection: Axis.horizontal,
-                    scrollPhysics: NeverScrollableScrollPhysics()),
-              )),
+          //  CS.CarouselSlider.builder(
+          //   itemCount: Helper.sliderItems.length,
+          //   itemBuilder: (context, index, pageindex) => Column(
+          //     children: [
+          //       const VerticalGap(
+          //         customGap: 30,
+          //       ),
+          //       Helper.sliderItems[index],
+          //       VerticalGap(),
+          //       Row(
+          //         mainAxisAlignment: MainAxisAlignment.center,
+          //         children: List.generate(
+          //             Helper.sliderItems.length,
+          //             (index) => Container(
+          //                   width: 20,
+          //                   height: 8,
+          //                   decoration: BoxDecoration(
+          //                       color: carosalIndex == index
+          //                           ? Config.logoColor
+          //                           : Config.logoColor.withOpacity(0.5),
+          //                       shape: BoxShape.circle),
+          //                 )).toList(),
+          //       )
+          //     ],
+          //   ),
+          //   options: CS.CarouselOptions(
+          //       height: 400,
+
+          //       // aspectRatio: 16 / 9,
+          //       viewportFraction: 0.8,
+          //       initialPage: 0,
+          //       enableInfiniteScroll: true,
+          //       reverse: false,
+          //       autoPlay: true,
+          //       autoPlayInterval: const Duration(seconds: 5),
+          //       autoPlayAnimationDuration: const Duration(seconds: 1),
+          //       autoPlayCurve: Curves.fastOutSlowIn,
+          //       enlargeCenterPage: true,
+          //       onPageChanged: (index, point) {
+          //         setState(() {
+          //           carosalIndex = index;
+          //         });
+          //       },
+          //       scrollDirection: Axis.horizontal,
+          //       scrollPhysics: NeverScrollableScrollPhysics()),
+          // )),
+
           const ContactUsSection(),
           const Footer()
         ],
